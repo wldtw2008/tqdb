861f9cf2b421c5b9297c118242d07af07d2bc282
tqdb <tqdb@TQDB>
2016-02-17 02:02:21 -0500

    [Bug] fix tiny bug and add InitialTQDB readme file

--- script_for_sys/tqdbStartup.sh
+++ script_for_sys/tqdbStartup.sh
@@ -4,7 +4,7 @@
 echo $CASS_IP":"$CASS_PORT > /tmp/cass.info
 echo $D2TQ_IP":"$D2TQ_PORT > /tmp/d2tq.info
 
-#cd $TQDB_DIR/script_for_sys && ./demo_d2tq_server.sh > /tmp/demo_d2tq_server.log &
+cd $TQDB_DIR/script_for_sys && ./demo_d2tq_server.sh > /tmp/demo_d2tq_server.log &
 
 sleep 10
 
