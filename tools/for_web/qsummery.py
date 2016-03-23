81b68843a473f8ed49b9218cb03b57ea7d77c8bc
wldtw2008 <wldtw2008@gmail.com>
2016-03-23 14:58:43 +0800

    [Summery] +symbol summery from web

---
+++ tools/for_web/qsummery.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python
+import time
+import sys
+import time
+import datetime
+from socket import socket
+import os
+import subprocess
+import json
+szCassIP1="127.0.0.1"
+szCassDB="tqdb1"
+
+
+def runCql(szCql, objRet):
+    p = subprocess.Popen(["/var/cassandra/bin/cqlsh", "-e", szCql], cwd="/var/cassandra/bin/",  
+                         stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    objRet['output'], objRet['err'] = p.communicate()
+    objRet['retcode'] = p.returncode
+
+querystrings=os.environ.get("QUERY_STRING", "NA=NA")
+#querystrings="sym=WTX&desc=Taifex&bpv=200&mko=84500&mkc=134500"
+mapQS={}
+for qs in querystrings.split("&"):
+        mapQS[qs.split("=")[0]] = qs.split("=")[1]
+
+sym='WTX'
+if 'sym' in mapQS: sym=mapQS['sym']
+summery={}
+if sym != '':
+	tmpFile="/tmp/qsummery.%d.%d.txt"%(os.getpid(),time.mktime(datetime.datetime.now().timetuple()))
+        objRet = {}
+	cql="select * from %s.tick where symbol='%s' order by datetime limit 10;" % (szCassDB, sym)
+        runCql(cql, objRet)
+        summery['tick.first'] = objRet['output']
+        cql="select * from %s.tick where symbol='%s' order by datetime desc limit 10;" % (szCassDB, sym)
+        runCql(cql, objRet)
+        summery['tick.last'] = objRet['output']
+
+        cql="select * from %s.secbar where symbol='%s' order by datetime limit 10;" % (szCassDB, sym)
+        runCql(cql, objRet)
+        summery['sec.first'] = objRet['output']
+        cql="select * from %s.secbar where symbol='%s' order by datetime desc limit 10;" % (szCassDB, sym)
+        runCql(cql, objRet)
+        summery['sec.last'] = objRet['output']
+
+        cql="select * from %s.minbar where symbol='%s' order by datetime limit 10;" % (szCassDB, sym)
+        runCql(cql, objRet)
+        summery['min.first'] = objRet['output']
+        cql="select * from %s.minbar where symbol='%s' order by datetime desc limit 10;" % (szCassDB, sym)
+        runCql(cql, objRet)
+        summery['min.last'] = objRet['output']
+
+        cql="select * from %s.symbol where symbol='%s';" % (szCassDB, sym)
+        runCql(cql, objRet)
+        summery['summery'] = objRet['output']
+
+sys.stdout.write("Content-Type: text/html\r\n")
+sys.stdout.write("\r\n")
+sys.stdout.write("<html>")
+sys.stdout.write("<style type='text/css'>")
+sys.stdout.write("<!--")
+sys.stdout.write("body { background: #FFF; font-family: 'Lucida Console', Monaco, monospace;}")
+sys.stdout.write("/-->")
+sys.stdout.write("</style>")
+sys.stdout.write("<body>")
+#sys.stdout.write("<script language='javascript'>location.href='/esymbol.html'</script>")
+sys.stdout.write("<table table border='1' style='border-collapse: collapse;' cellpadding='5'>")
+sys.stdout.write("<tr style='background-color:#ccc'><td colspan='2'>Summery</td></tr>")
+sys.stdout.write("<tr><td colspan='2'>%s</td></tr>"%summery['summery'].replace('\n','<br>'))
+
+sys.stdout.write("<tr style='background-color:#ccc'><td colspan='2'>Tick</td></tr>")
+sys.stdout.write("<tr><td>First 10 record</td><td>%s</td></tr><tr><td>Last 10 record</td><td>%s</td></tr>"%(summery['tick.first'].replace('\n','<br>'),summery['tick.last'].replace('\n','<br>')))
+
+sys.stdout.write("<tr style='background-color:#ccc'><td colspan='2'>Sec</td></tr>")
+sys.stdout.write("<tr><td>First 10 record</td><td>%s</td></tr><tr><td>Last 10 record</td><td>%s</td></tr>"%(summery['sec.first'].replace('\n','<br>'),summery['sec.last'].replace('\n','<br>')))
+
+sys.stdout.write("<tr style='background-color:#ccc'><td colspan='2'>Min</td></tr>")
+sys.stdout.write("<tr><td>First 10 record</td><td>%s</td></tr><tr><td>Last 10 record</td><td>%s</td></tr>"%(summery['min.first'].replace('\n','<br>'),summery['min.last'].replace('\n','<br>')))
+sys.stdout.write("</table>")
+sys.stdout.write("</body></html>")
+sys.stdout.write("\r\n")
+sys.stdout.flush()
---
+++ tools/src/TickTo1MinSec/formatDT.py
@@ -0,0 +1,40 @@
+import time
+import sys
+import time
+import datetime
+import re
+def formatDate(strDate):
+    iDate = 0
+    tmpSplits = strDate.split('/')
+    if len(tmpSplits) == 3:
+        iDate = int(tmpSplits[0])*10000+int(tmpSplits[1])*100+int(tmpSplits[2])
+        return str(iDate)
+
+    if len(strDate) == 8:
+        iDate = int(strDate)
+        return str(iDate)
+    return ""
+
+def formatTime(strTime):
+    hh,mm,ss,ms=(0,0,0,0)
+    tmpSplits = strTime.split(':')
+    if len(tmpSplits) == 3:
+        if tmpSplits[2].find('.')>0:
+            ssms=tmpSplits[2].split('.')
+            hh,mm,ss,ms=(int(tmpSplits[0]), int(tmpSplits[1]), int(ssms[0]), int(ssms[1]))
+        else:
+            hh,mm,ss,ms=(int(tmpSplits[0]), int(tmpSplits[1]), int(tmpSplits[2]), 0)
+
+    return "%02d%02d%02d.%d"%(hh,mm,ss,ms)
+
+def loopReadFromStdin():
+    for line in sys.stdin:
+        tags = line.strip().split(',')
+        tags[0] = formatDate(tags[0])
+        tags[1] = formatTime(tags[1])
+        if tags[0] == "": continue
+        for i in range(2, len(tags)):
+            tags[i] = str(float(tags[i]))
+        print ",".join(tags)
+
+loopReadFromStdin();
