2d7e0356d3ea115e267a8519853015f06d73764b
tqdb <tqdb@TQDB>
2016-06-04 16:27:22 +0800

    [q1min] add a param 'MOSTHAVEBEG', if there is no data during query range, it will return at last 1 bar before begDate

--- tools/for_web/q1min.py
+++ tools/for_web/q1min.py
@@ -6,18 +6,38 @@ import datetime
 from socket import socket
 import os
 import subprocess
-import urllib
+import urllib, urllib2, json
 
 szBinDir='/home/tqdb/codes/tqdb/tools/'
-szSymbol = "^^NKTOPX" #"SIN"; #"TXO;201506;9200P";
+szSymbol = "WTF.506" #"^^NKTOPX" #"SIN"; #"TXO;201506;9200P";
 timeoffset=0	
 iLocalTimeOff=480 #TW
 iGZip=1
 iRemoveQfile=1
-begDT = '2016-1-1'
-endDT = '2016-2-1'
+begDT = '2016-5-23 11:45:00'
+endDT = '2016-5-23 21:46:00'
 
 mapQS={}
+def getFirstValidDateTime(symbol, begDTstr, endDTstr):
+    begEOPCH, endEOPCH = (-1, -1) 
+    url = 'http://127.0.0.1/cgi-bin/qsummery.py?symbol=%s&qtype=qLastPrc&lastDT=%s' % (urllib2.quote(symbol), urllib2.quote(begDTstr))
+    resp = urllib2.urlopen(url)
+    obj = json.loads(resp.read())
+    #print obj
+    if obj != None and 'last' in obj and obj['last'][0] != None:
+        begEOPCH = obj['last'][1]/1000
+    url = 'http://127.0.0.1/cgi-bin/qsummery.py?symbol=%s&qtype=qLastPrc&lastDT=%s' % (urllib2.quote(symbol), urllib2.quote(endDTstr))
+    resp = urllib2.urlopen(url)
+    obj = json.loads(resp.read())
+    #print obj
+    if obj != None and 'last' in obj and obj['last'][0] != None:
+        endEOPCH = obj['last'][1]/1000
+    #print begEOPCH,endEOPCH 
+    if begEOPCH != -1 and begEOPCH == endEOPCH:
+        dtFirstValid = datetime.datetime.fromtimestamp(begEOPCH)
+        begDTstr = dtFirstValid.strftime("%Y-%m-%d %H:%M:%S")
+    return begDTstr
+
 def downloadFromTQDB(tmpFile):
 	subprocess.call("./q1minall.sh '%s' '%s' '%s' '%s' '%d'" % (szSymbol,begDT,endDT,tmpFile,iGZip), shell=True, cwd=szBinDir)
 
@@ -56,6 +76,8 @@ if ('BEG' in mapQS):
 	begDT=mapQS['BEG']
 if ('END' in mapQS):
 	endDT=mapQS['END']
+if ('MOSTHAVEBEG' in mapQS and mapQS['MOSTHAVEBEG'] != '0'):
+	begDT = getFirstValidDateTime(szSymbol, begDT, endDT)
 tmpFile="/tmp/q1min.%d.%d"%(os.getpid(),time.mktime(datetime.datetime.now().timetuple()))
 
 
