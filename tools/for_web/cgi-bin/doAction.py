5b3ec3bd9a9d7bbfab9596777278898470e16fe3
tqdb <wldtw2008@gmail.com>
2017-05-03 18:26:50 +0800

    add /cgi-bin/doAction.py

---
+++ tools/for_web/cgi-bin/doAction.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-   
+
+import sys,json,os,urllib, urllib2,cgi,cgitb
+from datetime import datetime
+from dateutil import tz, parser
+from cassandra.cluster import Cluster
+
+def _main(keyspace, cmd, params):
+    retObj = {'Result': 'Error! Wrong cmd!', 'Detail':None}
+    if cmd in ('TQALERT_MUTE', 'TQALERT_UNMUTE'):
+        retObj['Detail'] = {'Succeed':[], 'Failed':[]}
+        allSyms = params.split(',')
+        for sym in allSyms:
+            sym = sym.strip()
+            filename = '/tmp/watchTQ.skip.%s' % sym
+            try:
+                if cmd == 'TQALERT_MUTE': #MUTE!!
+                    with open(filename, 'w') as f:
+                        f.write('1\n')
+                else: #UN-MUTE
+                    os.remove(filename)
+                retObj['Detail']['Succeed'].append(sym)
+            except:
+                retObj['Detail']['Failed'].append(sym)
+                pass
+        retObj['Result'] = 'OK';
+    return retObj
+
+querystrings=os.environ.get("QUERY_STRING", "NA=NA")
+mapQS={}
+for qs in querystrings.split("&"):
+        if qs.find("=") <= 0: continue
+        mapQS[qs.split("=")[0]] = urllib.unquote(qs.split("=")[1])
+
+#debug
+mapQS['cmd'] = 'TQALERT_MUTE';
+mapQS['params']='WTX,WTE '
+
+allNeedParam = {'cmd':'', 'params':''}
+for key in allNeedParam.keys():
+    if key in mapQS:
+        allNeedParam[key] = mapQS[key]
+if (True):
+    form = cgi.FieldStorage()
+    for key in allNeedParam.keys():
+        valFromFS = form.getvalue(key)
+        if valFromFS is not None: allNeedParam[key] = valFromFS
+
+retObj = _main('tqdb1', allNeedParam['cmd'], allNeedParam['params'])
+
+sys.stdout.write("Content-Type: application/json; charset=UTF-8\r\n")
+sys.stdout.write("\r\n")
+sys.stdout.write(json.dumps(retObj))
+sys.stdout.flush()
+
