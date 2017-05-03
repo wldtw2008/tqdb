fbeea5ef875d9b43340dc2f9cee056147486f516
tqdb <wldtw2008@gmail.com>
2017-05-03 11:56:23 +0800

    TickQuote Alert

---
+++ tools/for_web/cgi-bin/eConf.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-   
+
+import sys,json,os,urllib, urllib2,cgi,cgitb
+from datetime import datetime
+from dateutil import tz, parser
+from cassandra.cluster import Cluster
+
+def _main(keyspace, confKey, confVal, cmd):
+    cluster = Cluster()
+    session = cluster.connect(keyspace)
+    qResult = None
+    if cmd in ('UPDATE'):
+        cqlCmd = "update %s.conf set confVal='%s' where confKey='%s'" % (keyspace, confVal.replace('"', '&quot;').replace('\'', '&apos;').replace('\\', '&bsol;'), confKey)
+        try:
+            eResult = session.execute(cqlCmd)
+        except:
+            return {'Result': 'Error! Failed to excute [%s]!'%cqlCmd}
+        return {'Result': 'OK'}
+    elif cmd in ('QUERY'):
+        queryStr = "select confVal from %s.conf where confKey='%s'" %(keyspace, confKey)
+        try:
+            qResult = session.execute(queryStr)
+        except:
+            return {'Result': 'Error! Failed to excute [%s]!'%queryStr}
+        if qResult is None or len(qResult.current_rows)<=0:
+            return {'Result': 'Error! No Such Data'}
+        return {'Result': 'OK', 'confVal':qResult[0][0].replace('&quot;', '"').replace('&apos;', '\'').replace('&bsol;', '\\')}
+
+    return {'Result': 'Error! Wrong cmd!'}
+
+querystrings=os.environ.get("QUERY_STRING", "NA=NA")
+mapQS={}
+for qs in querystrings.split("&"):
+        if qs.find("=") <= 0: continue
+        mapQS[qs.split("=")[0]] = urllib.unquote(qs.split("=")[1])
+
+#debug
+#mapQS['confKey'] = 'tqalert';
+#mapQS['cmd']='QUERY'
+
+allNeedParam = {'confKey':'', 'confVal':'', 'cmd':''}
+for key in allNeedParam.keys():
+    if key in mapQS:
+        allNeedParam[key] = mapQS[key]
+if (True):
+    form = cgi.FieldStorage()
+    for key in allNeedParam.keys():
+        valFromFS = form.getvalue(key)
+        if valFromFS is not None: allNeedParam[key] = valFromFS
+
+retObj = _main('tqdb1', allNeedParam['confKey'], allNeedParam['confVal'], allNeedParam['cmd'])
+
+sys.stdout.write("Content-Type: application/json; charset=UTF-8\r\n")
+sys.stdout.write("\r\n")
+sys.stdout.write(json.dumps(retObj))
+sys.stdout.flush()
+
