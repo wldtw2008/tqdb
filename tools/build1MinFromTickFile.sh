42619122716e02f0ce09087a779d521a2b04f389
tqdb <wldtw2008@gmail.com>
2019-07-01 11:03:27 +0800

    add build1MinFromTickFile.sh for restore 1Min bar from old-tick-files

---
+++ tools/build1MinFromTickFile.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ "$1" == "" ] ; then
+	echo "Systex Error!"
+	echo "Systex:  $0 [OldTickDir] [RestoreDate] [GO]"
+	echo "Example: $0 '/home/tqdb/oldtick/20190629.0/*.tick' 20190628 GO"
+	exit
+fi
+OLDTICK_FILES=$1
+RESTORE_DATE=$2
+GO=$3
+
+FILES=`ls ${OLDTICK_FILES}`
+
+for F in ${FILES}
+do
+    SYM=`basename ${F} .tick `
+    CMD="grep '^"${RESTORE_DATE}"' "${F}" | ./tick21min | python Min2Cass.py 127.0.0.1 9042 tqdb1 '"${SYM}"'"
+    echo "Ready to run: "${CMD}
+    if [ "${GO}" == "GO" ] ; then
+        eval ${CMD}
+    else
+        echo "    ---Simulate--- If you realy want to insert 1-Min data, Please put GO at 3rd parameter and run again."
+    fi
+    echo "    done."
+done
