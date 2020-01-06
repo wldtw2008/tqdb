2d63a256d852c52f506d5a46ea9382dedd0fd1ca
wldtw2008 <wldtw2008@gmail.com>
2020-01-06 05:28:31 +0000

    modify for support more datetime type when import 1min

--- tools/for_web/cgi-bin/i1min_check.py
+++ tools/for_web/cgi-bin/i1min_check.py
@@ -41,34 +41,53 @@ def _procPostData():
 
     if 'file' in form:
         fileitem = form['file']
-        iYYYYMMDDType = 0 #0=yyyymmdd 1=yyyy-mm-dd 2=yyyy/mm/dd
-        iHHMMSSType = 0 #0=hhmmss 1=hh:mm:ss
+        iYYYYMMDDType = 0 #0=yyyymmdd 1=yyyy-mm-dd 2=yyyy/mm/dd 3=mm/dd/yyyy
+        iHHMMSSType = 0 #0=hhmmss 1=hh:mm:ss 2=hh:mm
         while True:
             line = fileitem.file.readline()
             if not line: break
             onedata = line.strip('\n').strip('\r').split(',')
             if len(onedata)>=6:
-                try:#First item should be year
-                    year=int(onedata[0][0:4])
-                    if year<1900 or year>2030: continue
-                except:
-                    continue
-                #check if it is mc type
-                if len(param['Lines'])==0:
+                #first tag should be date (length more then 8 and first byte should be a number)
+                if len(onedata[0])<8: continue
+                if onedata[0][0] < '0' or onedata[0][0] > '9': continue
+
+                if len(param['Lines'])==0: #first line detacted date & time format
                     if len(onedata[0].split('-'))==3: iYYYYMMDDType=1
-                    if len(onedata[0].split('/'))==3: iYYYYMMDDType=2
+                    if len(onedata[0].split('/'))==3:
+                         iYYYYMMDDType=2
+                         try:
+                             if int(onedata[0].split('/')[2])>1900: iYYYYMMDDType=3
+                         except:
+                             pass
+
                     if len(onedata[1].split(':'))==3: iHHMMSSType=1
+                    if len(onedata[1].split(':'))==2: iHHMMSSType=2
+
                 if iYYYYMMDDType==1:
                     _date=onedata[0].split('-')
                     onedata[0]="%08d"%(int(_date[0])*10000+int(_date[1])*100+int(_date[2]))
                 elif iYYYYMMDDType==2:
                     _date=onedata[0].split('/')
                     onedata[0]="%08d"%(int(_date[0])*10000+int(_date[1])*100+int(_date[2]))
+                elif iYYYYMMDDType==3:
+                    _date=onedata[0].split('/')
+                    onedata[0]="%08d"%(int(_date[2])*10000+int(_date[0])*100+int(_date[1]))
+
                 if iHHMMSSType==1:
                     _time=onedata[1].split(':')
                     onedata[1]="%06d"%(int(_time[0])*10000+int(_time[1])*100+int(_time[2]))
-                if len(onedata) == 6: onedata.append('0') #for vol
+                elif iHHMMSSType==2:
+                    _time=onedata[1].split(':')
+                    onedata[1]="%06d"%(int(_time[0])*10000+int(_time[1])*100)
 
+                #try:#chech year
+                #    year=int(onedata[0][0:4])
+                #    if year<1900 or year>2030: continue
+                #except:
+                #    continue
+
+                if len(onedata) == 6: onedata.append('0') #for vol
                 param['Lines'].append(onedata)
 
     if param['tzFromTo'] is not None:
